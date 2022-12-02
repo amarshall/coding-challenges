3b88f0536f96fafb082f46920ebc3481a8bd0e6f
Andrew Marshall <andrew@johnandrewmarshall.com>
2022-12-01 22:54:35 -0500

    aoc-2022: Initial commit

---
+++ advent-of-code-2022/README.md
@@ -0,0 +1,7 @@
+# Advent of Code 2022 Solutions
+
+https://adventofcode.com/2022
+
+## License
+
+For all code, see LICENSE.txt file. All input.txt and problem.txt files are ⓒ Advent of Code, replicated here under fair use.
---
+++ advent-of-code-2022/flake.nix
@@ -0,0 +1,36 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/release-22.11";
+    devshell = {
+      url = "github:numtide/devshell";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    { self
+    , nixpkgs
+    , devshell
+    , flake-utils
+    }:
+    flake-utils.lib.eachDefaultSystem (system: (
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            devshell.overlay
+          ];
+        };
+      in
+      {
+        devShells.default = pkgs.devshell.mkShell {
+          motd = "";
+          devshell.packages = [
+            pkgs.hyperfine
+            pkgs.nixpkgs-fmt
+          ];
+        };
+      }
+    ));
+}
