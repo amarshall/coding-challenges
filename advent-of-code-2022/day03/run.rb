483f6a2684ce7da87bd4c0c0574cc84690c22d22
Andrew Marshall <andrew@johnandrewmarshall.com>
2022-12-06 08:49:37 -0500

    aoc-2022/day03: Initial solution

---
+++ advent-of-code-2022/day03/run.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/env ruby
+
+require 'pathname'
+
+input = Pathname.new(__dir__).join('./input.txt').freeze
+lines = input.readlines.each(&:freeze).freeze
+rucksacks = lines.map { |line| line.strip.split(//) }
+
+class Array
+  def split_at(idx)
+    [slice(0, idx), slice(idx, length - idx)]
+  end
+end
+
+priorities = Hash[(('a'..'z').to_a + ('A'..'Z').to_a).zip(1..Float::INFINITY)]
+
+part1 = rucksacks.flat_map do |rucksack_items|
+  compartment_1, compartment_2 = rucksack_items.split_at(rucksack_items.length / 2)
+  (compartment_1 & compartment_2).map { |item| priorities.fetch(item) }
+end.sum
+
+part2 = rucksacks.each_slice(3).flat_map do |group|
+  group.reduce(:&).map { |item| priorities.fetch(item) }
+end.sum
+
+puts "part1=#{part1} part2=#{part2}"
