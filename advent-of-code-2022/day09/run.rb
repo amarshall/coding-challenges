33fd6cc50dc89bac89cbe8ed3446aba771780bb1
Andrew Marshall <andrew@johnandrewmarshall.com>
2022-12-10 01:02:31 -0500

    aoc-2022/day09/part2: Initial solution

--- advent-of-code-2022/day09/run.rb
+++ advent-of-code-2022/day09/run.rb
@@ -11,40 +11,46 @@ class Numeric
   def sign; negative? ? -1 : 1 end
 end
 
-head = Coord.new(0, 0)
-tail = head.dup
+def run(actions, rope_length)
+  head, *, tail = rope = Array.new(rope_length) { Coord.new(0, 0) }
+  tail_positions = Set.new([tail.dup])
 
-tail_positions = Set.new([tail.dup])
+  actions.each do |direction, dist|
+    Integer(dist).times do
+      case direction
+      when ?L then head.x -= 1
+      when ?R then head.x += 1
+      when ?U then head.y -= 1
+      when ?D then head.y += 1
+      end
 
-input.each_line do |line|
-  direction, dist = line.match(/^([LRUD]) (\d+)$/).captures
-  dist = Integer(dist)
-  Integer(dist).times do
-    case direction
-    when ?L then head.x -= 1
-    when ?R then head.x += 1
-    when ?U then head.y -= 1
-    when ?D then head.y += 1
+      rope.each_cons(2) do |prev, this|
+        xΔ = prev.x - this.x
+        yΔ = prev.y - this.y
+
+        if [xΔ.abs, yΔ.abs].max > 1 && [xΔ.abs, yΔ.abs].min > 0
+          this.x += xΔ.sign
+          this.y += yΔ.sign
+        elsif xΔ.abs > 1
+          this.x += xΔ.sign
+        elsif yΔ.abs > 1
+          this.y += yΔ.sign
+        end
+      end
+
+      tail_positions << tail.dup
     end
-
-    xΔ = head.x - tail.x
-    yΔ = head.y - tail.y
-
-    if [xΔ.abs, yΔ.abs].max > 1 && [xΔ.abs, yΔ.abs].min > 0
-      tail.x += xΔ.sign
-      tail.y += yΔ.sign
-    elsif xΔ.abs > 1
-      tail.x += xΔ.sign
-    elsif yΔ.abs > 1
-      tail.y += yΔ.sign
-    end
-
-    tail_positions << tail.dup
   end
+
+  tail_positions
 end
 
-part1 = tail_positions.count
+actions = input.each_line.map do |line|
+  direction, dist = line.match(/^([LRUD]) (\d+)$/).captures
+  [direction, Integer(dist)].each(&:freeze).freeze
+end.freeze
 
-part2 = nil
+part1 = run(actions, 2).count
+part2 = run(actions, 10).count
 
 puts "part1=#{part1} part2=#{part2}"
