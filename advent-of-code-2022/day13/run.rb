a6ec4c5cafd3bf6ad559a216e476c8dfaf24eba1
Andrew Marshall <andrew@johnandrewmarshall.com>
2022-12-13 10:12:07 -0500

    aoc-2022/day13: Initial solution

---
+++ advent-of-code-2022/day13/run.rb
@@ -0,0 +1,33 @@
+#!/usr/bin/env ruby
+
+require 'pathname'
+
+input = Pathname.new(__dir__).join('./input.txt').freeze
+packets = input.readlines.map { |s| eval(s) }.compact
+
+def check_order(a, b)
+  case [a, b]
+  in [Integer, Integer] then a != b ? a < b : nil
+  in [Array, Array]
+    padding = Array.new([a,b].map(&:size).max, nil)
+    padding.zip(a, b).reduce(nil) do |_, (_, a, b)|
+      x = check_order(a, b)
+      break x unless x.nil?
+      nil
+    end
+  in [nil, _] then true
+  in [_, nil] then false
+  in [Array, Integer] then check_order(a, [b])
+  in [Integer, Array] then check_order([a], b)
+  end
+end
+
+part1 = packets.each_slice(2).map.with_index do |(a, b), idx|
+  check_order(a, b) ? idx+1 : nil
+end.compact.sum
+
+dividers = [[[2]], [[6]]]
+sorted = (packets + dividers).sort { |a, b| check_order(a, b) ? -1 : 1 }
+part2 = dividers.map { |divider| sorted.index(divider) + 1 }.reduce(:*)
+
+puts "part1:#{part1} part2:#{part2}"
